720ea1eb67fc176efbca78eba58744f77f51a802
drew-tom1 <tom.drew261@gmail.com>
2024-09-03 08:55:53 -0700

    minor changes

--- src/App.js
+++ src/App.js
@@ -35,7 +35,26 @@ function App() {
   
   return (
     <div>
-      <input onChange={(e) => setLocation(e.target.value)} />
+      <p>
+        The Product Manager Accelerator Program is designed to support PM professionals through every stage of their career. From students looking for entry-level jobs to Directors looking to take on a leadership role, our program has helped over hundreds of students fulfill their career aspirations.
+
+        Our Product Manager Accelerator community are ambitious and committed. Through our program they have learnt, honed and developed new PM and leadership skills, giving them a strong foundation for their future endeavours.
+
+        Learn product management for free today on our YouTube channel 
+        https://www.youtube.com/c/drnancyli?sub_confirmation=1
+
+        Interested in PM Accelerator Pro? 
+        Step 1️⃣: Attend the Product Masterclass to learn more about the program details, price, different packages, and stay until the end to get FREE  AI Course. 
+
+        Learn how to create a killer product portfolio 2 two weeks that will help you land any PM job( traditional or AI) even if you were laid off or have zero PM experience
+
+        https://www.drnancyli.com/masterclass
+
+        Step 2️⃣: Reserve your early bird ticket and submit an application to talk to our Head of Admission
+
+        Step 3️⃣: Successful applicants join our PMA Pro community to receive customized coaching!
+      </p>
+      <input onChange={(e) => setLocation(e.target.value)} placeholder='enter a city' />
       <button onClick={handleWeather}>Search</button>
       {weather && (
         <table>
